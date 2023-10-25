44dbfb455790988d71fabeedd205b4e9c7edf789
Javiersanjuan1 <fsanjua@myuax.com>
2023-10-25 11:05:46 +0200

    ejercicio 8

---
+++ ejercicio 8/constantes simbolicas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    const int LIMITE = 100;
+    const double PI = 3.14159;
+    const std::string MENSAJE = "Hola, mundo";
+
+    std::cout << "Límite: " << LIMITE << std::endl;
+    std::cout << "Valor de PI: " << PI << std::endl;
+    std::cout << "Mensaje: " << MENSAJE << std::endl;
+
+
+
+    return 0;
+}
